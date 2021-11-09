5e1e4ae5e6fbeb5f91d71ee51d61b4049a70e027
Pilar Figueroa <mpilarfigueroa.96@gmail.com>
2021-11-08 22:04:39 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+## Primera Parte
+- createElement
+- appendChild
+- setTimeout?
+
+## Segunda parte
+- array, que es que guarda
+- acceder a las posiciones de un array
+- donde encontramos arrays?
+- mostrar array por pantalla, como?
+- CICLO FOR
+- mostrar los elementos de mi array con el ciclo for
+- que puedo hacer dentro de un for? eventos dinamicos
---
+++ script.js
@@ -0,0 +1,50 @@
+// VARIABLES GLOBALES
+// const btn = document.querySelector('#btn');
+// const contenedor = document.querySelector('#contenedor-cajas');
+
+// const crearCaja = () => {
+//     const div = document.createElement('div');
+//     div.style.width = '200px';
+//     div.style.height = '200px';
+//     div.style.backgroundColor = 'red';
+//     div.style.margin = '20px';
+//     contenedor.style.display = 'flex';
+//     contenedor.appendChild(div);
+// }
+
+// btn.onclick = crearCaja;
+
+// const pedirFrutas = () => {
+//     let fruta = prompt('Ingrese una fruta');
+//     const ul = document.createElement('ul');
+//     const li = document.createElement('li');
+//     li.innerText = fruta;
+//     ul.appendChild(li);
+//     contenedor.appendChild(ul);
+// }
+
+// btn.onclick = pedirFrutas;
+
+// const contenedorFrutas = document.querySelector('#contenedor-frutas')
+// const frutas = ["naranja", "banana", "pera", "manzana"];
+// console.log(frutas[3])
+
+//    variable
+
+// const listarFrutas = () => {
+//     for (let i = 0; i < 5; i++) {
+//         let fruta = prompt('Ingrese una fruta')
+//         let li = document.createElement('li');
+//         li.innerText = fruta;
+//         contenedorFrutas.appendChild(li);
+//     }
+// }
+
+// const botones = document.querySelectorAll('.btn');
+// const color = ['red', 'green', 'blue', 'yellow', 'brown']
+
+// for (let i = 0; i < botones.length; i++) {
+//     botones[i].onclick = () => {
+//         botones[i].style.backgroundColor = `${color[i]}`
+//     }
+// }
