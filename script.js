c238f4b012b71f88a48f2e56811cbca2e5d1f2ba
Pilar Figueroa <55300926+PiliFigueroa@users.noreply.github.com>
2021-11-10 14:39:07 -0300

    agregando ejercicios

--- script.js
+++ script.js
@@ -48,3 +48,24 @@
 //         botones[i].style.backgroundColor = `${color[i]}`
 //     }
 // }
+
+/* EJERCICIO 1
+Crear una pagina con un formulario de un input y un boton enviar. El input debe admitir solo 1 caracter en el cual el usuario ingresara una letra.
+Si la letra es una vocal, al hacer click en enviar se debe imprimir un texto en verde que diga "Vocal detectada".
+Si no es una vocal, se debe imprimir un texto en rojo que diga "Consonante detectada".
+- Nota: si utilizas un if, solo puede haber if y else, no utilizar elseif. Recordar operadores logicos.
+*/
+
+/* EJERCICIO 2
+Crear un programa en el que se pueda ingresar por un input una temperatura en celcius. Al hacer click en un boton "Convertir" se debe
+imprimir en pantalla su equivalente en farengeit.
+*/
+
+/* EJERCICIO 3
+Crear un programa que pida al usuario ingresar la temperatura actual. Al ingresarla se debe imprimir un texto que diga
+"La temperatura es (temperatura ingresada por el usuario)". El color del texto dependera de la temperatura:
+- de -50 a 15 en AZUL
+- de 15 a 21 en AMARILLO
+- de 21 a 30 en NARANJA
+- de 30 a 40 en ROJO
+*/
