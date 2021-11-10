359cdbf6e07b7e650bc1798093db199db84cc442
Pilar Figueroa <55300926+PiliFigueroa@users.noreply.github.com>
2021-11-10 14:42:49 -0300

    agregando ejercicios 4 y 5

--- script.js
+++ script.js
@@ -69,3 +69,13 @@ Crear un programa que pida al usuario ingresar la temperatura actual. Al ingresa
 - de 21 a 30 en NARANJA
 - de 30 a 40 en ROJO
 */
+
+/* EJERCICIO 4
+Crear un programa con un input de tipo color. Segun el color que elige el usuario, se debe cambiar el color de fondo del documento al
+color que eligio al usuario.
+*/
+
+/* EJERCICIO 5
+Crear un programa que cambie de color una caja de 200x200 pixeles al hacer click en la misma. Inicialmente, la caja sera roja. Al darle click
+debe cambiar a azul, al darle click otra vez, cambiar a verde, y por ultimo a amarillo.
+*/
